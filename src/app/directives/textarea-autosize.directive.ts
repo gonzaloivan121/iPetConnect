cc36c9cad7fd78a81e994e6c9cb6beda8f8e1437
Gonzalo Iván Chaparro Barese <gonzaloivan121@gmail.com>
2023-12-31 20:41:16 +0100

    Created autosize directive for textareas

---
+++ src/app/directives/textarea-autosize.directive.ts
@@ -0,0 +1,28 @@
+import { Directive, HostListener, ElementRef, OnInit } from '@angular/core';
+
+@Directive({
+    selector: '[autosize]'
+})
+export class TextareaAutosizeDirective implements OnInit {
+
+    private initialHeight: string;
+
+    constructor(
+        private elementRef: ElementRef
+    ) { }
+
+    @HostListener(':input')
+    onInput() {
+        this.resize();
+    }
+
+    ngOnInit() {
+        this.initialHeight = this.elementRef.nativeElement.style.height;
+    }
+
+    resize() {
+        this.elementRef.nativeElement.style.height = this.initialHeight;
+        this.elementRef.nativeElement.style.height = this.elementRef.nativeElement.scrollHeight + 'px';
+    }
+
+}
