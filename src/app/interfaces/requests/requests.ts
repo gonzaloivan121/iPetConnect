6287ad207d4ec5b3ad96ff2b421bcfc8af233da5
Gonzalo Iván Chaparro Barese <gonzaloivan121@gmail.com>
2023-12-31 20:41:45 +0100

    Added a requests interface

---
+++ src/app/interfaces/requests/requests.ts
@@ -0,0 +1,25 @@
+export interface IMessageRequest {
+    chat_id: number;
+    user_id: number;
+    message: string;
+    edited?: boolean;
+    read?: boolean;
+}
+
+export interface IMessageResponse {
+    success: boolean;
+    message: string;
+    result?: IResponseResult;
+    created_at?: string;
+}
+
+export interface IResponseResult {
+    affectedRows: number;
+    changedRows: number;
+    fieldCount: number;
+    insertId: number;
+    message: string;
+    protocol41: boolean;
+    serverStatus: number;
+    warningCount: number;
+}
