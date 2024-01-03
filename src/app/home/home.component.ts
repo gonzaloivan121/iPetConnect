f13bb95f1c43d7cf0b78b836a6b81c687e7877f6
Gonzalo Iván Chaparro Barese <gonzaloivan121@gmail.com>
2024-01-03 04:24:43 +0100

    Added admin button on home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { SessionService } from '../services';
+import { SessionService } from 'src/app/services';
+import { RoleEnum } from 'src/app/enums/enums';
+import { User } from 'src/classes';
 
 @Component({
     selector: 'app-home',
@@ -13,10 +15,13 @@ export class HomeComponent implements OnInit {
     ) { }
 
     isLoggedIn: boolean = false;
+    isAdmin: boolean = false;
 
     ngOnInit() {
         if (this.sessionService.get('user') !== null) {
             this.isLoggedIn = true;
+            var user: User = JSON.parse(this.sessionService.get('user'));
+            this.isAdmin = user.role_id === RoleEnum.Admin;
         }
     }
 }
