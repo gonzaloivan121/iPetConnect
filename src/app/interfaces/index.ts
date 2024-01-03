8002423a34b583d277f008cf6c2551e5123ad4ff
Gonzalo Iván Chaparro Barese <gonzaloivan121@gmail.com>
2024-01-03 14:07:14 +0100

    Added MapLegendIcon interface

--- src/app/interfaces/index.ts
+++ src/app/interfaces/index.ts
@@ -1,3 +1,4 @@
 export { AppConfig, DBConfig } from './app-config/app-config';
 export { IMessageRequest, IMessageResponse, IResponseResult, IMarkerResponse } from './requests/requests';
 export { ISocialMediaLink } from './social-media/social-media'
+export { IMapLegendIcon } from './map-legend/map-legend';
---
+++ src/app/interfaces/map-legend/map-legend.ts
@@ -0,0 +1,5 @@
+export interface IMapLegendIcon {
+    url: string;
+    type: string;
+    active: boolean;
+}
