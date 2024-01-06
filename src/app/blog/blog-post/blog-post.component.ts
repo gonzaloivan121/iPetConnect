a4692e067e60697683c36c4071d19929eada6cb8
Gonzalo Iván Chaparro Barese <gonzaloivan121@gmail.com>
2024-01-06 03:25:00 +0100

    Blog WIP

--- src/app/blog/blog-post/blog-post.component.ts
+++ src/app/blog/blog-post/blog-post.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnDestroy, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
-import { Subscription } from "rxjs";
-import { DataService } from "src/app/services";
-import { BlogPost, DBTables } from "src/classes";
-import { IBlogPostResponse } from "src/app/interfaces";
-import Glide from "@glidejs/glide";
+import { Observable, Subscription, from, of } from "rxjs";
+import { catchError, map } from "rxjs/operators";
+import { DataService, SessionService } from "src/app/services";
+import { DBTables, User } from "src/classes";
+import { IBlogPostResponse, IBlogPost, IBlogComment } from "src/app/interfaces";
+import { UntypedFormBuilder, UntypedFormGroup, Validators } from "@angular/forms";
 
 @Component({
     selector: "app-blog-post",
@@ -13,42 +14,151 @@ import Glide from "@glidejs/glide";
 })
 export class BlogPostComponent implements OnInit, OnDestroy {
     routeSubscription: Subscription;
-    post: BlogPost;
+    post: IBlogPost;
+    user?: User;
+    postUser: User;
+    postComments: IBlogComment[];
+
+    public postLoaded: Observable<boolean>;
+    public postUserLoaded: Observable<boolean>;
+    public postCommentsLoaded: Observable<boolean>;
+
+    public commentForm: UntypedFormGroup;
+    public isCommentFocused: boolean = false;
 
     constructor(
-        private activatedRoute: ActivatedRoute,
-        private dataService: DataService
-    ) {
-        this.activatedRoute.params.subscribe((params) => console.log(params));
-    }
+        private route: ActivatedRoute,
+        private dataService: DataService,
+        private sessionService: SessionService,
+        private formBuilder: UntypedFormBuilder
+    ) {}
 
     ngOnInit(): void {
-        this.routeSubscription = this.activatedRoute.params.subscribe(
-            (params) => {
-                const id = params["id"];
-                this.dataService
-                    .get(DBTables.BlogPost, id)
-                    .then((response: IBlogPostResponse) => {
-                        if (response.success) {
-                            if (response.result.length > 0) {
-                                this.post = response.result[0] as BlogPost;
-                            } else {
-                                console.warn("Empty data!");
-                            }
-                        } else {
-                            console.error(response.message);
-                        }
-                    })
-                    .catch((error) => console.error(error));
+        if (this.sessionService.get("user")) {
+            this.user = JSON.parse(this.sessionService.get("user"));
+        }
+
+        this.routeSubscription = this.route.params.subscribe(
+            (params: { id: number }) => {
+                const id = params.id;
+                this.postLoaded = this.loadPost(id);
             }
         );
 
-        new Glide(".glide", {
-            type: "carousel",
-            perView: 4,
-            startAt: 2,
-            focusAt: 2,
-        }).mount();
+        this.commentForm = this.formBuilder.group({
+            comment: ["", [Validators.required]],
+        });
+    }
+
+    get comment() {
+        return this.commentForm.get("comment");
+    }
+
+    onSubmit() {
+        const formData = this.commentForm.value;
+        const data: IBlogComment = {
+            content: formData.comment,
+            post_id: this.post.id,
+            user_id: this.user.id,
+            likes: 0
+        };
+
+        this.insertComment(data);
+    }
+
+    insertComment(data: IBlogComment) {
+        this.dataService.insert(DBTables.BlogComment, data).then((response: any) => {
+            if (response.success) {
+                const comment = data;
+                comment.id = response.result.insertId;
+                comment.created_at = response.created_at;
+                comment.updated_at = response.updated_at;
+
+                this.postComments.push(comment);
+
+                this.comment.setValue(null);
+            } else {
+                console.error(response.message);
+            }
+        }).catch((error) => console.error(error));
+    }
+
+    loadPost(id: number): Observable<boolean> {
+        return from(
+            this.dataService
+                .get(DBTables.BlogPost, id)
+                .then((response: IBlogPostResponse) => {
+                    if (response.success) {
+                        if (response.result.length > 0) {
+                            this.post = response.result[0] as IBlogPost;
+                            this.postUserLoaded = this.loadPostUser(
+                                this.post.user_id
+                            );
+                            this.postCommentsLoaded = this.loadPostComments(
+                                this.post.id
+                            );
+                        } else {
+                            console.warn("Empty data!");
+                        }
+                    } else {
+                        console.error(response.message);
+                    }
+                })
+                .catch((error) => console.error(error))
+        ).pipe(
+            map(() => true),
+            catchError(() => of(false))
+        );
+    }
+
+    loadPostUser(id: number): Observable<boolean> {
+        return from(
+            this.dataService
+                .get(DBTables.User, id)
+                .then((response: any) => {
+                    if (response.success) {
+                        this.postUser = response.result[0] as User;
+                    } else {
+                        console.error(response.message);
+                    }
+                })
+                .catch((error) => console.error(error))
+        ).pipe(
+            map(() => true),
+            catchError(() => of(false))
+        );
+    }
+
+    loadPostComments(id: number): Observable<boolean> {
+        return from(
+            this.dataService
+                .getFrom(DBTables.BlogComment, DBTables.BlogPost, id)
+                .then((response: any) => {
+                    if (response.success) {
+                        this.postComments = response.result as IBlogComment[];
+                        console.log(this.postComments);
+                    } else {
+                        console.error(response.message);
+                    }
+                })
+                .catch((error) => console.error(error))
+        ).pipe(
+            map(() => true),
+            catchError(() => of(false))
+        );
+    }
+
+    likeComment(comment: IBlogComment) {
+        comment.likes++;
+        this.dataService.update(DBTables.BlogComment, comment).then((response: any) => {
+            console.log(response)
+
+            if (response.success) {
+
+            } else {
+
+            }
+        }).catch((error) => console.error(error));
     }
 
     ngOnDestroy(): void {
--- src/app/blog/categories/categories.component.ts
+++ src/app/blog/categories/categories.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { IBlogCategory } from "src/app/interfaces";
+import { DataService } from "src/app/services";
+import { DBTables } from "src/classes";
 
 @Component({
-  selector: 'app-categories',
-  templateUrl: './categories.component.html',
-  styleUrls: ['./categories.component.css']
+    selector: "app-categories",
+    templateUrl: "./categories.component.html",
+    styleUrls: ["./categories.component.css"],
 })
 export class CategoriesComponent implements OnInit {
 
-  constructor() { }
+    public categories: IBlogCategory[];
 
-  ngOnInit(): void {
-  }
+    constructor(private dataService: DataService) {}
 
+    ngOnInit(): void {
+        this.dataService.get(DBTables.BlogCategory + "/latest/3").then((response: any) => {
+            if (response.success) {
+                this.categories = response.result as IBlogCategory[];
+            }
+        });
+    }
 }
