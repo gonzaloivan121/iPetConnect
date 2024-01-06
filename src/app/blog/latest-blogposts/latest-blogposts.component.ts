4da4c204a9a07c53a473225b5159cf76fc8d56f9
Gonzalo Iván Chaparro Barese <gonzaloivan121@gmail.com>
2024-01-06 19:19:37 +0100

    Select latest blog posts from database

--- src/app/blog/latest-blogposts/latest-blogposts.component.ts
+++ src/app/blog/latest-blogposts/latest-blogposts.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Observable, from, of } from "rxjs";
+import { catchError, map } from "rxjs/operators";
+import { DataService } from "src/app/services";
+import { DBTables } from "src/classes";
+import { IBlogPost } from "src/app/interfaces";
 
 @Component({
-  selector: 'app-latest-blogposts',
-  templateUrl: './latest-blogposts.component.html',
-  styleUrls: ['./latest-blogposts.component.css']
+    selector: "app-latest-blogposts",
+    templateUrl: "./latest-blogposts.component.html",
+    styleUrls: ["./latest-blogposts.component.css"],
 })
 export class LatestBlogpostsComponent implements OnInit {
+    posts: IBlogPost[] = [];
 
-  constructor() { }
+    public postsLoaded: Observable<boolean>;
 
-  ngOnInit(): void {
-  }
+    constructor(private dataService: DataService) {}
 
+    ngOnInit(): void {
+        this.postsLoaded = this.loadPosts();
+    }
+
+    loadPosts(): Observable<boolean> {
+        return from(
+            this.dataService
+                .get(DBTables.BlogPost + "/latest")
+                .then((response: any) => {
+                    if (response.success) {
+                        this.posts = response.result as IBlogPost[];
+                    } else {
+                        console.error(response.message);
+                    }
+                })
+                .catch((error) => console.error(error))
+        ).pipe(
+            map(() => true),
+            catchError(() => of(false))
+        );
+    }
 }
