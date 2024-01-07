3a70db706cd93cb0bb91826c18c27396bd97bea7
Gonzalo Iván Chaparro Barese <gonzaloivan121@gmail.com>
2024-01-07 02:18:18 +0100

    Added working newsletter

--- src/app/blog/newsletter/newsletter.component.ts
+++ src/app/blog/newsletter/newsletter.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { EmailService } from 'src/app/services';
+import { Component, OnInit } from "@angular/core";
+import { UntypedFormGroup, UntypedFormBuilder, Validators } from "@angular/forms";
+import { IEmail } from "src/app/interfaces";
 
 @Component({
-  selector: 'app-newsletter',
-  templateUrl: './newsletter.component.html',
-  styleUrls: ['./newsletter.component.css']
+    selector: "app-newsletter",
+    templateUrl: "./newsletter.component.html",
+    styleUrls: ["./newsletter.component.css"],
 })
 export class NewsletterComponent implements OnInit {
+    newsletterForm: UntypedFormGroup;
 
-  constructor() { }
+    focusEmail: boolean;
 
-  ngOnInit(): void {
-  }
+    constructor(
+        private formBuilder: UntypedFormBuilder,
+        private emailService: EmailService
+    ) {}
 
+    ngOnInit(): void {
+        this.newsletterForm = this.formBuilder.group({
+            email: ["", [Validators.required, Validators.email]],
+        });
+    }
+
+    get email() {
+        return this.newsletterForm.get("email");
+    }
+
+    onSubmit() {
+        const formData = this.newsletterForm.value;
+
+        const data: IEmail = {
+            email: formData.email,
+            name: "Newsletter",
+            message: "Newsletter Subscription for " + formData.email
+        };
+
+        this.emailService.Send(data).then((response: any) => {
+            console.log(response);
+        })
+    }
 }
