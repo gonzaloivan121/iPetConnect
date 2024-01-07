de7bd9e6350f769d5f28ea9e45b56df7171b5f1d
Gonzalo Iván Chaparro Barese <gonzaloivan121@gmail.com>
2024-01-07 01:08:10 +0100

    Modified and parameterized the-team section

--- src/app/blog/the-team/the-team.component.ts
+++ src/app/blog/the-team/the-team.component.ts
@@ -1,15 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import { ITeamMember } from 'src/app/interfaces';
 
 @Component({
-  selector: 'app-the-team',
-  templateUrl: './the-team.component.html',
-  styleUrls: ['./the-team.component.css']
+    selector: "app-the-team",
+    templateUrl: "./the-team.component.html",
+    styleUrls: ["./the-team.component.css"],
 })
 export class TheTeamComponent implements OnInit {
+    team: ITeamMember[] = [
+        {
+            id: 1,
+            name: "Gonzalo Chaparro",
+            position: "UX Designer",
+            aptitudes: {
+                business: "Dedicated entrepreneur",
+                leisure: "Outdoors lover",
+                personal: "Super friendly support team",
+            },
+            image: "assets/img/team/Gonzalo.jpg",
+        },
+        {
+            id: 2,
+            name: "Gonzalo Chaparro",
+            position: "Team Lead",
+            aptitudes: {
+                business: "Dedicated entrepreneur",
+                leisure: "Gamer",
+                personal: "Able to get good at everything",
+            },
+            image: "assets/img/team/Gonzalo.jpg",
+        },
+        {
+            id: 3,
+            name: "Gonzalo Chaparro",
+            position: "Content Creator",
+            aptitudes: {
+                business: "High quality publication",
+                leisure: "Storytelling",
+                personal: "Master of words qualification",
+            },
+            image: "assets/img/team/Gonzalo.jpg",
+        },
+    ];
 
-  constructor() { }
+    constructor() {}
 
-  ngOnInit(): void {
-  }
-
+    ngOnInit(): void {}
 }
