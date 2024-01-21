ac5677d13b5fe947d824b5a4605e268920ea8e10
Gonzalo Iván Chaparro Barese <gonzaloivan121@gmail.com>
2024-01-21 16:44:23 +0100

    Modified strings

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,23 +4,22 @@ import { RoleEnum } from 'src/app/enums/enums';
 import { User } from 'src/classes';
 
 @Component({
-    selector: 'app-home',
-    templateUrl: './home.component.html',
-    styleUrls: ['./home.component.scss']
+    selector: "app-home",
+    templateUrl: "./home.component.html",
+    styleUrls: ["./home.component.scss"],
 })
-
 export class HomeComponent implements OnInit {
     constructor(
         private sessionService: SessionService
-    ) { }
+    ) {}
 
     isLoggedIn: boolean = false;
     isAdmin: boolean = false;
 
     ngOnInit() {
-        if (this.sessionService.get('user') !== null) {
+        if (this.sessionService.get("user") !== null) {
             this.isLoggedIn = true;
-            var user: User = JSON.parse(this.sessionService.get('user'));
+            var user: User = JSON.parse(this.sessionService.get("user"));
             this.isAdmin = user.role_id === RoleEnum.Admin;
         }
     }
