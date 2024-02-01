d6464e6919f21c8c95e6a03716d1f5cd9c5d8bee
Gonzalo Iván Chaparro Barese <gonzaloivan121@gmail.com>
2024-02-01 13:23:03 +0100

    Fixed datepicker only showing -10 years from present

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -6,22 +6,29 @@ import { DBTables } from 'src/classes';
 import { IConfig, IUser } from 'src/app/interfaces';
 import { Page } from 'src/app/enums/enums';
 
+interface IDatepickerDate {
+    year: number,
+    month: number,
+    day: number
+}
+
 @Component({
-    selector: 'app-signup',
-    templateUrl: './signup.component.html',
-    styleUrls: ['./signup.component.scss']
+    selector: "app-signup",
+    templateUrl: "./signup.component.html",
+    styleUrls: ["./signup.component.scss"],
 })
 export class SignupComponent implements OnInit {
     signupForm: UntypedFormGroup;
-    maxBirthDate: {
-        year: number,
-        month: number,
-        day: number
-    } = {
+    minBirthDate: IDatepickerDate = {
+        year: new Date().getFullYear() - 100,
+        month: new Date().getMonth() + 1,
+        day: new Date().getDate(),
+    };
+    maxBirthDate: IDatepickerDate = {
         year: new Date().getFullYear(),
         month: new Date().getMonth() + 1,
-        day: new Date().getDate()
-    }
+        day: new Date().getDate(),
+    };
 
     focusUsername;
     focusName;
@@ -48,28 +55,43 @@ export class SignupComponent implements OnInit {
     }
 
     ngOnInit() {
-        if (this.sessionService.get('user') !== null) {
-            this.location.go('/home');
+        if (this.sessionService.get("user") !== null) {
+            this.location.go("/home");
             window.location.reload();
         }
 
-        this.signupForm = this.formBuilder.group({
-            username: ["", [Validators.required, Validators.minLength(8)]],
-            email: ["", [Validators.required, Validators.email]],
-            password: ["", [Validators.required, Validators.minLength(8), this.createPasswordStrengthValidator()]],
-            confirmPassword: ["", [Validators.required]],
-            name: ["", [Validators.required]],
-            role_id: [2, [Validators.required]],
-            birthday: ["", [Validators.required, this.ageGreaterThanMinRequired()]],
-            gender: ["", [Validators.required]],
-            agreePrivacyPolicy: [false, [Validators.requiredTrue]]
-        }, { validators: this.confirmPasswordValidator() });
+        this.signupForm = this.formBuilder.group(
+            {
+                username: ["", [Validators.required, Validators.minLength(8)]],
+                email: ["", [Validators.required, Validators.email]],
+                password: [
+                    "",
+                    [
+                        Validators.required,
+                        Validators.minLength(8),
+                        this.createPasswordStrengthValidator(),
+                    ],
+                ],
+                confirmPassword: ["", [Validators.required]],
+                name: ["", [Validators.required]],
+                role_id: [2, [Validators.required]],
+                birthday: [
+                    "",
+                    [Validators.required, this.ageGreaterThanMinRequired()],
+                ],
+                gender: ["", [Validators.required]],
+                agreePrivacyPolicy: [false, [Validators.requiredTrue]],
+            },
+            { validators: this.confirmPasswordValidator() }
+        );
     }
 
     confirmPasswordValidator(): ValidatorFn {
         return (control: AbstractControl): ValidationErrors | null => {
-            return control.value.password === control.value.confirmPassword ? null : { passwordNoMatch: true };
-        }
+            return control.value.password === control.value.confirmPassword
+                ? null
+                : { passwordNoMatch: true };
+        };
     }
 
     createPasswordStrengthValidator(): ValidatorFn {
@@ -85,53 +107,60 @@ export class SignupComponent implements OnInit {
             const hasNumeric = /[0-9]+/.test(value);
             const hasSpecial = /[!@#$%&*()_+=|<>?{}\[\]~-]+/.test(value);
 
-            const isValid = hasUpperCase && hasLowerCase && hasNumeric && hasSpecial;
+            const isValid =
+                hasUpperCase && hasLowerCase && hasNumeric && hasSpecial;
 
-            const validArray = [hasUpperCase, hasLowerCase, hasNumeric, hasSpecial];
+            const validArray = [
+                hasUpperCase,
+                hasLowerCase,
+                hasNumeric,
+                hasSpecial,
+            ];
             this.passwordStrength = validArray.filter(Boolean).length;
 
-            return !isValid ? {
-                passwordStrength: {
-                    hasUpperCase: !hasUpperCase,
-                    hasLowerCase: !hasLowerCase,
-                    hasNumeric: !hasNumeric,
-                    hasSpecial: !hasSpecial
-                }
-            } : null;
-
-        }
+            return !isValid
+                ? {
+                      passwordStrength: {
+                          hasUpperCase: !hasUpperCase,
+                          hasLowerCase: !hasLowerCase,
+                          hasNumeric: !hasNumeric,
+                          hasSpecial: !hasSpecial,
+                      },
+                  }
+                : null;
+        };
     }
 
     get username() {
-        return this.signupForm.get('username');
+        return this.signupForm.get("username");
     }
 
     get email() {
-        return this.signupForm.get('email');
+        return this.signupForm.get("email");
     }
 
     get password() {
-        return this.signupForm.get('password');
+        return this.signupForm.get("password");
     }
 
     get confirmPassword() {
-        return this.signupForm.get('confirmPassword');
+        return this.signupForm.get("confirmPassword");
     }
 
     get name() {
-        return this.signupForm.get('name');
+        return this.signupForm.get("name");
     }
 
     get role_id() {
-        return this.signupForm.get('role_id');
+        return this.signupForm.get("role_id");
     }
 
     get birthday() {
-        return this.signupForm.get('birthday');
+        return this.signupForm.get("birthday");
     }
 
     get gender() {
-        return this.signupForm.get('gender');
+        return this.signupForm.get("gender");
     }
 
     onSubmit() {
@@ -139,32 +168,35 @@ export class SignupComponent implements OnInit {
         formData.birthday = `${formData.birthday.year}/${formData.birthday.month}/${formData.birthday.day}`;
         formData.birthday = new Date(formData.birthday);
         formData.image = "assets/img/ipetconnect_background.jpg";
-        
-        this.dataService.insert(DBTables.User, formData).then((response: any) => {
-            if (response.success) {
-                const user: IUser = {
-                    id: response.result.insertId,
-                    username: formData.username,
-                    email: formData.email,
-                    password: response.hash,
-                    name: formData.name,
-                    role_id: formData.role_id,
-                    birthday: formData.birthday,
-                    gender: formData.gender,
-                    image: formData.image,
-                    created_at: response.created_at,
-                    updated_at: response.created_at,
-                };
 
-                this.sessionService.set('user', JSON.stringify(user));
+        this.dataService
+            .insert(DBTables.User, formData)
+            .then((response: any) => {
+                if (response.success) {
+                    const user: IUser = {
+                        id: response.result.insertId,
+                        username: formData.username,
+                        email: formData.email,
+                        password: response.hash,
+                        name: formData.name,
+                        role_id: formData.role_id,
+                        birthday: formData.birthday,
+                        gender: formData.gender,
+                        image: formData.image,
+                        created_at: response.created_at,
+                        updated_at: response.created_at,
+                    };
 
-                this.generateConfigForUser(user);
-            } else {
-                console.warn(response.message);
-            }
-        }).catch((error) => {
-            console.error(error);
-        });
+                    this.sessionService.set("user", JSON.stringify(user));
+
+                    this.generateConfigForUser(user);
+                } else {
+                    console.warn(response.message);
+                }
+            })
+            .catch((error) => {
+                console.error(error);
+            });
     }
 
     generateConfigForUser(user: IUser) {
@@ -180,25 +212,28 @@ export class SignupComponent implements OnInit {
             search_in_distance: serviceData.onlySearchDistanceRange,
             search_in_age: serviceData.onlySearchAgeRange,
             search_has_bio: serviceData.onlySearchHasBioUsers,
-            user_id: user.id
+            user_id: user.id,
         };
 
-        this.dataService.insert(DBTables.Config, data).then((response: any) => {
-            if (response.success) {
-                this.location.go("/home");
-                window.location.reload();
-            }
-        }).catch((error) => {
-            console.error(error);
-        });
+        this.dataService
+            .insert(DBTables.Config, data)
+            .then((response: any) => {
+                if (response.success) {
+                    this.location.go("/home");
+                    window.location.reload();
+                }
+            })
+            .catch((error) => {
+                console.error(error);
+            });
     }
 
     ageGreaterThanMinRequired(): ValidatorFn {
-        return (control: AbstractControl) : ValidationErrors | null => {
+        return (control: AbstractControl): ValidationErrors | null => {
             const value: {
-                year: number,
-                month: number,
-                day: number
+                year: number;
+                month: number;
+                day: number;
             } = control.value;
 
             if (!value) return null;
@@ -210,12 +245,15 @@ export class SignupComponent implements OnInit {
             const diffMs = Math.abs(today.getTime() - birthday.getTime());
             const diffYears = Math.floor(diffMs / (1000 * 60 * 60 * 24 * 365));
 
-            return diffYears < this.configService.data.minAgePossible ? {
-                ageRequired: {
-                    minAgePossible: this.configService.data.minAgePossible
-                }
-            } : null;
-        }
+            return diffYears < this.configService.data.minAgePossible
+                ? {
+                      ageRequired: {
+                          minAgePossible:
+                              this.configService.data.minAgePossible,
+                      },
+                  }
+                : null;
+        };
     }
 
     toggleShowPassword() {
